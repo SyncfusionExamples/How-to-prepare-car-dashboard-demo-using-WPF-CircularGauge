ebf5fe77d72238a90d6e9338abec6a273541d587
EswaranSyncfusion <eswaran.thirugnanasambandam@syncfusion.com>
2022-01-28 16:48:19 +0530

    Cardashboard sample added

---
+++ CarDashBaordSample/GaugeGettingStarted/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Syncfusion.UI.Xaml.Gauges;
+
+namespace GaugeGettingStarted
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
